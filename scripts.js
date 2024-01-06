e95806bcc12da0801b2d908bdc03c07181a16aa8
mounikasangana0126 <sssmounika5@gmail.com>
2024-01-06 17:56:47 +0530

    new commit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     document.getElementsByClassName("form-section").display
-// })
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("form-section").style.display = "none";
+    document.getElementById("edit-details").addEventListener("click", () => {
+        
+        document.getElementById("form-section").style.display = "revert";
+        document.getElementById("registered-details-section").style.display = "none";
+        document.getElementById("head-section-content").style.display = "none";
+        document.getElementById("courses").style.display = "none";
+    })
+    document.getElementById("close-details").addEventListener("click", () => {
+        
+        document.getElementById("form-section").style.display = "none";
+        document.getElementById("registered-details-section").style.display = "revert";
+        document.getElementById("head-section-content").style.display = "revert";
+        document.getElementById("courses").style.display = "revert";
+    })
+
+    
+})
