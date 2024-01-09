a2f6ed3e20c459843b11fe82fab10401c672e96f
kritirajmaharana <124510753+kritirajmaharana@users.noreply.github.com>
2024-01-09 14:46:24 +0530

    changes done

--- scripts.js
+++ scripts.js
@@ -2,17 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("form-section").style.display = "none";
     document.getElementById("edit-details").addEventListener("click", () => {
         
-        document.getElementById("form-section").style.display = "revert";
-        document.getElementById("registered-details-section").style.display = "none";
-        document.getElementById("head-section-content").style.display = "none";
-        document.getElementById("courses").style.display = "none";
+        document.getElementById("form-section").style.display = "block";
+        document.getElementById("blackshadow").style.display="block";
     })
     document.getElementById("close-details").addEventListener("click", () => {
         
         document.getElementById("form-section").style.display = "none";
-        document.getElementById("registered-details-section").style.display = "revert";
-        document.getElementById("head-section-content").style.display = "revert";
-        document.getElementById("courses").style.display = "revert";
+        document.getElementById("blackshadow").style.display="none";
+
+       
     })
 
     
