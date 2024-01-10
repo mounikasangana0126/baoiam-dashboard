141fdd8bcdd56e681aafe874618be7499c80523f
mounikasangana0126 <sssmounika5@gmail.com>
2024-01-10 11:17:18 +0530

    responsive page

--- scripts.js
+++ scripts.js
@@ -1,16 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("form-section").style.display = "none";
+
+    //edit details button
     document.getElementById("edit-details").addEventListener("click", () => {
         
         document.getElementById("form-section").style.display = "block";
         document.getElementById("blackshadow").style.display="block";
     })
+
+    //close details button
     document.getElementById("close-details").addEventListener("click", () => {
         
         document.getElementById("form-section").style.display = "none";
         document.getElementById("blackshadow").style.display="none";
 
        
+    })
+
+    //for media queries:up arrow to display main contain 
+    document.querySelector(".links .down").addEventListener("click", () => {
+        document.getElementById("aside").style.display = "none";
+        document.querySelector(".main .down").style.display = "flex";
+
+    })
+
+    //for media queries:down arrow to display aside contain 
+    document.querySelector(".main .down").addEventListener("click", () => {
+        document.getElementById("aside").style.display = "block";
+        document.querySelector(".main .down").style.display = "none";
+
     })
 
     
